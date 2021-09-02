b2adc666201ea60a79cc2987bc11d9c6c34d38d6
chloeouyang <rouyang2@babson.edu>
2021-09-02 19:48:24 -0400

    part of Question3

--- calc.py
+++ calc.py
@@ -1,7 +1,16 @@
 # 2.1
+print('Question1')
 print(42*60+42)
 
 # 2.2
-print(10*1.61)
+print('Question2')
+print(10/1.61)
 
 # 2.3
+print('Question3')
+
+mile=10/1.61
+min=42/60
+sec=42/60/60
+hour=min+sec
+print(mile/hour)
