f757db6b861e45a6d09d0836553b081051279d6c
chloeouyang <rouyang2@babson.edu>
2021-09-03 11:59:55 -0400

    Finish the exercise 2

--- calc.py
+++ calc.py
@@ -10,7 +10,18 @@ print(10/1.61)
 print('Question3')
 
 mile=10/1.61
+secinmin=42/60
+mins=42+secinmin
+print(mile/mins)
+print('This is average speed per minute.')
+mininsec=42*60
+secs=42+mininsec
+print(mile/secs)
+print('This is average speed per second.')
 min=42/60
 sec=42/60/60
 hour=min+sec
 print(mile/hour)
+print('This is average speed per hour.')
+
+print((10/1.61)/(42/60+42/60/60))
