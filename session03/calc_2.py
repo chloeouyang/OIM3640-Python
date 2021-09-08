d1736343047ef4b34b9995ca7a0812ddee7a2592
chloeouyang <rouyang2@babson.edu>
2021-09-08 00:00:15 -0400

    exercise02 Q1and2

---
+++ session03/calc_2.py
@@ -0,0 +1,14 @@
+# 1.
+pi = 3.1415926
+# pi = int(pi)
+print((4 / 3) * pi * 5 ** 3)
+# if int is added, the answer would be 500, which makes pi equal to 3
+# when do we need int() and when not? the number assigned to variables is integer but the input must be string right?
+
+# 2.
+x = 60
+print(24.95 * 0.6 * x + 3 + 0.75 * (x - 1))
+
+# 3.
+
+
