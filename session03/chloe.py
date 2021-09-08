87944b9dae5f061d3d4f5703ca4112df6103abff
chloeouyang <rouyang2@babson.edu>
2021-09-07 23:31:57 -0400

    session03 class

--- session03/chloe.py
+++ session03/chloe.py
@@ -1,11 +1,34 @@
-print('Hey Jude','don\'t make it bad')
+print("Hey Jude", "don't make it bad")
 
-first_name = 'chloe'
-last_name = 'ouyang'
-print(first_name+' '+last_name)
-print(f'{first_name} {last_name}')
+first_name = "chloe"
+last_name = "ouyang"
+print(first_name + " " + last_name)
+print(f"{first_name} {last_name}")
+print(first_name, last_name)
 
-print('chloe ouyang'*10)
-print('chloe ouyang \n'*10)
+print("chloe ouyang" * 10)
+print("chloe ouyang \n" * 10)
 
+pi = 3.1415926
+print(f"Pi equals {pi:.5f}.")
+print("Pi equals", pi)
+print(f"Pi equals {pi}.")
+print(f"Pi equals {pi:8.5f}.")
+print(f"pi equals {pi:8.2f}.")
+print(f"pi equals {pi:17.5f}.")
+print(f"pi equals {pi:17.2f}.")
 
+a=2021
+print(f'{a:b}')
+print(f'{a:x}')
+print(f'{a:o}')
+print(f'{a:e}')
+
+s1='a'
+s2='ab'
+s3='abc'
+s4='abcd'
+print(f'{s1:>10}')
+print(f'{s2:>10}')
+print(f'{s3:>10}')
+print(f'{s4:>10}')
