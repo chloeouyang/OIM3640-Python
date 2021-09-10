035727bd2f99bf3cdf19de35a9923bfc9a85b158
chloeouyang <rouyang2@babson.edu>
2021-09-10 13:13:22 -0400

    Update exercise04.py

--- session04/exercise04.py
+++ session04/exercise04.py
@@ -1,22 +1,3 @@
-# 3.
-# import datetime
-# movingtime= (8*60+15)*2+(7*60+12)*3
-# t=datetime.datetime(2021,9,8,6,52)
-# print (t)
-# h=movingtime//60
-# m=movingtime%60
-# print (h)
-# print (m)
-# print (t+datetime.timedelta(hours=h,minutes=m))
-
-# newt=t+datetime.timedelta(hours=h,minutes=m)
-# print(newt.time())
-
-# # 4.
-# percentageinnumber=(89-82)/82
-# percentage=percentageinnumber*100
-# print(f'{percentage:.1f}%')
-
 import time
 from typing import Mapping
 print(time.time())
