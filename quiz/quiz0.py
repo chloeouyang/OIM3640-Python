a1862a06fbd9b5a11436493a0641f91b1d84525e
chloeouyang <rouyang2@babson.edu>
2021-09-21 19:05:39 -0400

    quiz0 finish

--- quiz/quiz0.py
+++ quiz/quiz0.py
@@ -10,3 +10,16 @@ Notice:
 2. Your function should include docstring.
 3. Write your own test code, i.e. call the function.
 """
+
+# get the weight of the user on earth
+weight = input("What is your weight on earth?>>>")
+# change the datatype into int
+weight = int(weight)
+# define the function
+def moon(weight):
+    """get the weight on the moon by calculating the weight on the earth"""
+    weight_on_moon = weight * 0.165
+    return weight_on_moon
+
+
+print(moon(weight))
