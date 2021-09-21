2a616a8f2db5981b1cd254f86ffedd3d197b3a12
chloeouyang <rouyang2@babson.edu>
2021-09-21 19:30:49 -0400

    quiz0 finish2

--- session07/exercise07.py
+++ session07/exercise07.py
@@ -3,4 +3,4 @@ def check_fermat(a,b,c,n):
     if n>2 and a**n+b**n=c**n:
         print('Holy smokes,Fermat was wrong!')
     else:
-        print('No, that doesn't work')
+        print('No, that doesn\'t work')
