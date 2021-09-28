61cfb2832bebd7b55dbcec4b265df373976c6de8
chloeouyang <rouyang2@babson.edu>
2021-09-27 22:45:03 -0400

    session08 finish(format)

--- session08/exercise03.py
+++ session08/exercise03.py
@@ -10,18 +10,21 @@ def mysqrt(a):
         x = y
     return y
 
+
 # print(mysqrt(25))
+
 
 def test_square_root():
     print("a   mysqrt(a)     math.sqrt(a)  diff")
     print("-   ---------     ------------  ----")
-    for i in range(1,10):
-        a=int(i)
-        b=mysqrt(a)
-        c=math.sqrt(a)
-        d=abs(b-c)
-        print(f'{a}{b:<17.12}{c:<17.12}{d}')
+    for i in range(1, 10):
+        a = int(i)
+        b = mysqrt(a)
+        c = math.sqrt(a)
+        d = abs(b - c)
+        print(f"{a}{b:<17.12}{c:<17.12}{d}")
+
 
 test_square_root()
 
-#question about aligning the data
+# question about aligning the data
